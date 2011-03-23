70bbbb6890f1163f08313e767cc7284e847f3ec8
Richard Sbresny <voidet@nightgen.com>
2011-03-23 16:30:38 +1000

    Included proper HABTM with field in the array keys

--- models/behaviors/tag_time.php
+++ models/behaviors/tag_time.php
@@ -52,7 +52,7 @@ class TagTimeBehavior extends ModelBehavior {
 
 			 	if (!empty($tagIds)) {
 					foreach($tagIds as $key => $tagId) {
-						$Model->data[$assoc_key][][$assoc_model['associationForeignKey']] = $tagId;
+						$Model->data[$assoc_key][][$assoc_model['with']][$assoc_model['associationForeignKey']] = $tagId;
 						unset($Model->data[$assoc_key][$form_field]);
 					}
 				}
